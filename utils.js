bf93b4bc0e7bc4140a63c541a224494c88677a7d
u2007007 <u2007007@student.cuet.ac.bd>
2026-05-15 17:01:11 +0600

    Update and rename test.js to utils.js

---
+++ utils.js
@@ -0,0 +1,9 @@
+/**
+ * Adds two numbers together.
+ * @param a - First number
+ * @param b - Second number
+ * @returns Sum of a and b
+ */
+export function add(a: number, b: number): number {
+  return a + b;
+}
