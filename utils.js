db14938c2618e5f2e181e800c68a4e84ba085503
u2007007 <u2007007@student.cuet.ac.bd>
2026-05-18 15:52:36 +0600

    Fix add function signature and implementation

--- utils.js
+++ utils.js
@@ -4,6 +4,6 @@
  * @param b - Second number
  * @returns Sum of a and b
  */
-export function add(a: number,b:number:c:number): number {
-  return a + b + c  ;
+export function add(a: number,b:number): number {
+  return a + b  ;
 }
