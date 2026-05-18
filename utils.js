01aed6fd74dea1bc28eebe4444cfd16fdfd10347
u2007007 <u2007007@student.cuet.ac.bd>
2026-05-18 16:27:58 +0600

    Refactor add function to take two arguments
    Updated the add function to accept only two parameters.

--- utils.js
+++ utils.js
@@ -6,6 +6,6 @@
  * @param c - Third number
  * @returns Sum of a, b, and c
  */
-export function add(a: number,b:number,c:number): number {
-  return a + b + c ;
+export function add(a: number,b:number): number {
+  return a + b;
 }
