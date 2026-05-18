873d07c63842010e3f7d17852d8691cd87776879
u2007007 <u2007007@student.cuet.ac.bd>
2026-05-18 16:30:24 +0600

    Modify add function to include a third parameter
    Updated the add function to accept a third parameter and return the sum of three numbers.

--- utils.js
+++ utils.js
@@ -3,8 +3,9 @@
  * Adds two numbers together.
  * @param a - First number
  * @param b - Second number
+ * @param c - Third number
  * @returns Sum of a and b
  */
-export function add(a: number,b:number): number {
-  return a + b;
+export function add(a: number,b:number,c:number): number {
+  return a + b + c;
 }
